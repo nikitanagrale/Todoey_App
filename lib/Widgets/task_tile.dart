690c9868fe9a227892385dbf5ce3fd1b15c83e74
Nikhil Nagrale <nknagrale634@gmail.com>
2020-09-07 00:30:00 +0530

    checkbox toggle added

--- lib/Widgets/task_tile.dart
+++ lib/Widgets/task_tile.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('task 1'),
-      trailing: Checkbox(
-        value: false,
+      title: Text(
+        'task 1',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckbox(
+        checkBoxState: isChecked,
+        toggleCheckBox: (bool checkBoxState) {
+          setState(() {
+            isChecked = checkBoxState;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function toggleCheckBox;
+
+  TaskCheckbox({this.checkBoxState, this.toggleCheckBox});
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkBoxState,
+      onChanged: toggleCheckBox,
     );
   }
 }
