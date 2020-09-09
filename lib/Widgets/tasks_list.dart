bcdbec0db293fc0e64f69edb1dbc4cc27dc8e4e9
Nikhil Nagrale <nknagrale634@gmail.com>
2020-09-09 19:26:18 +0530

    check off method added

--- lib/Widgets/tasks_list.dart
+++ lib/Widgets/tasks_list.dart
@@ -9,13 +9,12 @@ class TasksList extends StatelessWidget {
     return Consumer<TaskData>(builder: (context, taskData, child) {
       return ListView.builder(
         itemBuilder: (context, index) {
+          final task = taskData.tasks[index];
           return TaskTile(
-            isChecked: taskData.tasks[index].isDone,
-            taskTitle: taskData.tasks[index].name,
+            isChecked: task.isDone,
+            taskTitle: task.name,
             checkBoxCallback: (bool checkBoxState) {
-//            setState(() {
-//              widget.tasks[index].toggleDone();
-//            });
+              taskData.updateTask(task);
             },
           );
         },
